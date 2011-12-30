81105099413f03ce754bcfe1b4831b66922582a0
Marcos de Vera Piquero <mdevera@gisce.net>
2011-12-30 17:13:51 +0100

    IMP Ocultem els internals de la facturacio
    Fem que, quan es faci ús de libcomxml.messages.facturacio, no
    calgui saber l'estructura interna. Fins ara calia passar-li el nom
    del tag i el root de l'element. Ara això ho manega 'facturacio'.
    
    Aquest canvi implica un canvi en el constructor, per tant fa els
    missatges de facturació incompatibles cap enrera.

--- libcomxml/messages/facturacio.py
+++ libcomxml/messages/facturacio.py
@@ -9,7 +9,7 @@ class Cabecera(XmlModel):
     _sort_order = ('cabecera', 'ree_emisora', 'ree_destino', 'proceso', 'paso',
                    'solicitud', 'secuencia', 'codigo', 'fecha', 'version')
 
-    def __init__(self, name, root):
+    def __init__(self):
         self.cabecera = XmlField('Cabecera')
         self.ree_emisora = XmlField('CodigoREEEmpresaEmisora')
         self.ree_destino = XmlField('CodigoREEEmpresaDestino')
@@ -20,14 +20,14 @@ class Cabecera(XmlModel):
         self.codigo = XmlField('Codigo')
         self.fecha = XmlField('FechaSolicitud')
         self.version = XmlField('Version', value='02')
-        super(Cabecera, self).__init__(name, root)
+        super(Cabecera, self).__init__('Cabecera', 'cabecera')
 
 
 class DatosGeneralesFactura(XmlModel):
     _sort_order = ('datos', 'numero', 'tipo', 'rectificadora', 'fecha', 'cif',
                    'codigo', 'obs', 'importe', 'saldo', 'saldocobro', 'moneda')
 
-    def __init__(self, name, root):
+    def __init__(self):
         self.datos = XmlField('DatosGeneralesFactura')
         self.numero = XmlField('NumeroFactura')
         self.tipo = XmlField('TipoFactura')
@@ -40,119 +40,123 @@ class DatosGeneralesFactura(XmlModel):
         self.saldo = XmlField('SaldoFactura')
         self.saldocobro = XmlField('SaldoCobro')
         self.moneda = XmlField('TipoMoneda', value='02')
-        super(DatosGeneralesFactura, self).__init__(name, root)
+        super(DatosGeneralesFactura, self).__init__('DatosGeneralesFactura',
+                                                    'datos')
 
 
 class Periodo(XmlModel):
     _sort_order = ('periodo', 'fecha_desde', 'fecha_hasta', 'meses')
 
-    def __init__(self, name, root):
+    def __init__(self):
         self.periodo = XmlField('Periodo')
         self.fecha_desde = XmlField('FechaDesdeFactura')
         self.fecha_hasta = XmlField('FechaHastaFactura')
         self.meses = XmlField('NumeroMeses')
-        super(Periodo, self).__init__(name, root)
+        super(Periodo, self).__init__('Periodo', 'periodo')
 
 
 class DatosFacturaATR(XmlModel):
     _sort_order = ('datos', 'tipo', 'boe', 'tarifa', 'imab', 'periodo')
 
-    def __init__(self, name, root):
+    def __init__(self):
         self.datos = XmlField('DatosFacturaATR')
         self.tipo = XmlField('TipoFacturacion')
         self.boe = XmlField('FechaBOE')
         self.tarifa = XmlField('CodigoTarifa')
         self.imab = XmlField('IndAltamedidoenBaja')
         self.periodo = Periodo('Periodo', root='periodo')
-        super(DatosFacturaATR, self).__init__(name, root)
+        super(DatosFacturaATR, self).__init__('DatosFacturaATR', 'datos')
 
 
 class DireccionSuministro(XmlModel):
     _sort_order = ('direccion', 'cups', 'municipio', 'dirsuministro')
 
-    def __init__(self, name, root):
+    def __init__(self):
         self.direccion = XmlField('DireccionSuministro')
         self.cups = XmlField('CUPS')
         self.municipio = XmlField('Municipio')
         self.dirsuministro = XmlField('DirSuministro')
-        super(DireccionSuministro, self).__init__(name, root)
+        super(DireccionSuministro, self).__init__('DireccionSuministro',
+                                                  'direccion')
 
 class Cliente(XmlModel):
     _sort_order = ('cliente', 'cifnif', 'identificador')
 
-    def __init__(self, name, root):
+    def __init__(self):
         self.cliente = XmlField('Cliente')
         self.cifnif = XmlField('TipoCIFNIF')
         self.identificador = XmlField('Identificador')
-        super(Cliente, self).__init__(name, root)
+        super(Cliente, self).__init__('Cliente', 'cliente')
 
 class DatosGeneralesFacturaATR(XmlModel):
     _sort_order = ('datos', 'direccion', 'cliente', 'contrato', 'datosgrles',
                    'datosatr')
 
-    def __init__(self, name, root):
+    def __init__(self):
         self.datos = XmlField('DatosGeneralesFacturaATR')
         self.direccion = DireccionSuministro('DireccionSuministro', root='direccion')
         self.cliente = Cliente('Cliente', root='cliente')
         self.contrato = XmlField('Contrato')
         self.datosgrles = DatosGeneralesFactura('DatosGeneralesFactura', root='datos')
         self.datosatr = DatosFacturaATR('DatosFacturaATR', root='datos')
-        super(DatosGeneralesFacturaATR, self).__init__(name, root)
+        super(DatosGeneralesFacturaATR,
+              self).__init__('DatosGeneralesFacturaATR', 'datos')
 
 class TerminoPotencia(XmlModel):
     _sort_order = ('termino', 'fecha_desde', 'fecha_hasta', 'periodos')
 
-    def __init__(self, name, root):
+    def __init__(self):
         self.termino = XmlField('TerminoPotencia')
         self.fecha_desde = XmlField('FechaDesde')
         self.fecha_hasta = XmlField('FechaHasta')
         self.periodos = []
-        super(TerminoPotencia, self).__init__(name, root)
+        super(TerminoPotencia, self).__init__('TerminoPotencia', 'termino')
 
 
 class Potencia(XmlModel):
     _sort_order = ('potencia', 'termino', 'icp', 'importe')
 
-    def __init__(self, name, root):
+    def __init__(self):
         self.potencia = XmlField('Potencia')
         self.termino = TerminoPotencia('TerminoPotencia', root='termino')
         self.icp = XmlField('PenalizacionNoICP')
         self.importe = XmlField('ImporteTotalTerminoPotencia')
-        super(Potencia, self).__init__(name, root)
+        super(Potencia, self).__init__('Potencia', 'potencia')
 
 class FacturaATR(XmlModel):
     _sort_order = ('factura', 'datosatr', 'potencia')
 
-    def __init__(self, name, root):
+    def __init__(self):
         self.factura = XmlField('FacturaATR')
         self.datosatr = DatosGeneralesFacturaATR('DatosGeneralesFacturaATR',
                                             root='datos')
         self.potencia = Potencia('Potencia', root='potencia')
-        super(FacturaATR, self).__init__(name, root)
+        super(FacturaATR, self).__init__('FacturaATR', 'factura')
 
 class PeriodoPotencia(XmlModel):
     _sort_order = ('periodo', 'contratada', 'maxdemandada', 'afacturar',
                    'precio')
 
-    def __init__(self, name, root):
+    def __init__(self):
         self.periodo = XmlField('Periodo')
         self.contratada = XmlField('PotenciaContratada')
         self.maxdemandada = XmlField('PotenciaMaxDemandada')
         self.afacturar = XmlField('PotenciaAFacturar')
         self.precio = XmlField('PrecioPotencia')
-        super(PeriodoPotencia, self).__init__(name, root)
+        super(PeriodoPotencia, self).__init__('PeriodoPotencia', 'periodo')
 
 
 class MensajeFacturacion(XmlModel):
     _sort_order = ('mensaje', 'cabecera', 'facturas')
 
-    def __init__(self, name, root):
+    def __init__(self):
         self.doc_root = None
         self.mensaje = XmlField('MensajeFacturacion', attributes={
                            'xmlns': 'http://localhost/elegibilidad'})
         self.cabecera = Cabecera('Cabecera', root='cabecera')
         self.facturas = XmlField('Facturas')
-        super(MensajeFacturacion, self).__init__(name, root)
+        super(MensajeFacturacion, self).__init__('MensajeFacturacion',
+                                                 'mensaje')
 
     def set_agente(self, agente):
         self.mensaje.attributes.update({'AgenteSolicitante': agente})
