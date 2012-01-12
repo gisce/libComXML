4c95175f8b1447dd0ab8f7893e1cd47b941b44af
Marcos de Vera Piquero <mdevera@gisce.net>
2012-01-12 13:46:30 +0100

    IMP Canvis capçaleres i nous models

--- libcomxml/messages/facturacio.py
+++ libcomxml/messages/facturacio.py
@@ -2,6 +2,7 @@
 
 # pylint: disable=E1002
 # pylint: disable=E1101
+# pylint: disable=C0111
 
 from ..core import XmlModel, XmlField
 
@@ -64,7 +65,7 @@ class DatosFacturaATR(XmlModel):
         self.boe = XmlField('FechaBOE')
         self.tarifa = XmlField('CodigoTarifa')
         self.imab = XmlField('IndAltamedidoenBaja')
-        self.periodo = Periodo('Periodo', root='periodo')
+        self.periodo = Periodo()
         super(DatosFacturaATR, self).__init__('DatosFacturaATR', 'datos')
 
 
@@ -94,11 +95,11 @@ class DatosGeneralesFacturaATR(XmlModel):
 
     def __init__(self):
         self.datos = XmlField('DatosGeneralesFacturaATR')
-        self.direccion = DireccionSuministro('DireccionSuministro', root='direccion')
-        self.cliente = Cliente('Cliente', root='cliente')
+        self.direccion = DireccionSuministro()
+        self.cliente = Cliente()
         self.contrato = XmlField('Contrato')
-        self.datosgrles = DatosGeneralesFactura('DatosGeneralesFactura', root='datos')
-        self.datosatr = DatosFacturaATR('DatosFacturaATR', root='datos')
+        self.datosgrles = DatosGeneralesFactura()
+        self.datosatr = DatosFacturaATR()
         super(DatosGeneralesFacturaATR,
               self).__init__('DatosGeneralesFacturaATR', 'datos')
 
@@ -112,26 +113,83 @@ class TerminoPotencia(XmlModel):
         self.periodos = []
         super(TerminoPotencia, self).__init__('TerminoPotencia', 'termino')
 
+# TODO Migrate 'termino's to a list
 
 class Potencia(XmlModel):
     _sort_order = ('potencia', 'termino', 'icp', 'importe')
 
     def __init__(self):
         self.potencia = XmlField('Potencia')
-        self.termino = TerminoPotencia('TerminoPotencia', root='termino')
+        self.termino = TerminoPotencia()
         self.icp = XmlField('PenalizacionNoICP')
         self.importe = XmlField('ImporteTotalTerminoPotencia')
         super(Potencia, self).__init__('Potencia', 'potencia')
 
+class ExcesoPotencia(XmlModel):
+    _sort_order = ('exceso', 'periodos', 'importe')
+
+    def __init__(self):
+        self.exceso = XmlField('ExcesoPotencia')
+        self.periodos = []
+        self.importe = XmlField('ImporteTotalExcesos')
+        super(ExcesoPotencia, self).__init__('ExcesoPotencia', 'exceso')
+
+
+class TerminoEnergiaActiva(XmlModel):
+    _sort_order = ('termino', 'fecha_desde', 'fecha_hasta', 'periodos')
+
+    def __init__(self):
+        self.termino = XmlField('TerminoEnergiaActiva')
+        self.fecha_desde = XmlField('FechaDesde')
+        self.fecha_hasta = XmlField('FechaHasta')
+        self.periodos = []
+        super(TerminoEnergiaActiva, self).__init__(
+            'TerminoEnergiaActiva', 'termino')
+
+
+class EnergiaActiva(XmlModel):
+    _sort_order = ('activa', 'termino', 'importe')
+
+    def __init__(self):
+        self.activa = XmlField('EnergiaActiva')
+        self.termino = TerminoEnergiaActiva()
+        self.importe = XmlField('ImporteTotalEnergiaActiva')
+        super(EnergiaActiva, self).__init__('EnergiaActiva', 'activa')
+
+
+class TerminoEnergiaReactiva(XmlModel):
+    _sort_order = ('termino', 'fecha_desde', 'fecha_hasta', 'periodos')
+
+    def __init__(self):
+        self.termino = XmlField('TerminoEnergiaReactiva')
+        self.fecha_desde = XmlField('FechaDesde')
+        self.fecha_hasta = XmlField('FechaHasta')
+        self.periodos = []
+        super(TerminoEnergiaReactiva, self).__init__(
+            'TerminoEnergiaReactiva', 'termino')
+
+
+class EnergiaReactiva(XmlModel):
+    _sort_order = ('reactiva', 'termino', 'importe')
+
+    def __init__(self):
+        self.reactiva = XmlField('EnergiaReactiva')
+        self.termino = TerminoEnergiaActiva()
+        self.importe = XmlField('ImporteTotalEnergiaReactiva')
+        super(EnergiaReactiva, self).__init__('EnergiaRectiva', 'reactiva')
+
+
 class FacturaATR(XmlModel):
-    _sort_order = ('factura', 'datosatr', 'potencia')
+    _sort_order = ('factura', 'datosatr', 'potencia', 'energia', 'reactiva')
 
     def __init__(self):
         self.factura = XmlField('FacturaATR')
-        self.datosatr = DatosGeneralesFacturaATR('DatosGeneralesFacturaATR',
-                                            root='datos')
-        self.potencia = Potencia('Potencia', root='potencia')
+        self.datosatr = DatosGeneralesFacturaATR()
+        self.potencia = Potencia()
+        self.energia = EnergiaActiva()
+        self.reactiva = EnergiaReactiva()
         super(FacturaATR, self).__init__('FacturaATR', 'factura')
+
 
 class PeriodoPotencia(XmlModel):
     _sort_order = ('periodo', 'contratada', 'maxdemandada', 'afacturar',
@@ -146,6 +204,49 @@ class PeriodoPotencia(XmlModel):
         super(PeriodoPotencia, self).__init__('PeriodoPotencia', 'periodo')
 
 
+class PeriodoEnergiaActiva(XmlModel):
+    _sort_order = ('periodo', 'valor', 'precio')
+
+    def __init__(self):
+        self.periodo = XmlField('Periodo')
+        self.valor = XmlField('ValorEnergiaActiva')
+        self.precio = XmlField('PrecioEnergia')
+        super(PeriodoEnergiaActiva, self).__init__('PeriodoEnergiaActiva',
+                                                   'periodo')
+
+
+class PeriodoEnergiaReactiva(XmlModel):
+    _sort_order = ('periodo', 'valor', 'precio')
+
+    def __init__(self):
+        self.periodo = XmlField('Periodo')
+        self.valor = XmlField('ValorEnergiaReactiva')
+        self.precio = XmlField('PrecioEnergiaReactiva')
+        super(PeriodoEnergiaReactiva, self).__init__('PeriodoEnergiaReactiva',
+                                                     'periodo')
+
+
+class ImpuestoElectrico(XmlModel):
+    _sort_order = ('iese', 'base', 'coef', 'percent', 'importe')
+
+    def __init__(self):
+        self.iese = XmlField('ImpuestoElectrico')
+        self.base = XmlField('BaseImponible')
+        self.coef = XmlField('Coeficiente')
+        self.percent = XmlField('Porcentaje')
+        self.importe = XmlField('Importe')
+        super(ImpuestoElectrico, self).__init__('ImpuestoElectrico', 'iese')
+
+
+class Alquileres(XmlModel):
+    _sort_order = ('alquileres', 'importe')
+
+    def __init__(self):
+        self.alquileres = XmlField('Alquileres')
+        self.importe = XmlField('ImporteFacturacionAlquileres')
+        super(Alquileres, self).__init__('Alquileres', 'alquileres')
+
+
 class MensajeFacturacion(XmlModel):
     _sort_order = ('mensaje', 'cabecera', 'facturas')
 
@@ -153,7 +254,7 @@ class MensajeFacturacion(XmlModel):
         self.doc_root = None
         self.mensaje = XmlField('MensajeFacturacion', attributes={
                            'xmlns': 'http://localhost/elegibilidad'})
-        self.cabecera = Cabecera('Cabecera', root='cabecera')
+        self.cabecera = Cabecera()
         self.facturas = XmlField('Facturas')
         super(MensajeFacturacion, self).__init__('MensajeFacturacion',
                                                  'mensaje')
