49f01c60a10368c8760750f33202a2a216747d3b
Carles Pey <cpey@gisce.net>
2012-09-24 10:48:01 +0200

    BUMP into v1.2.3

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='libComXML',
-    version='1.2.2',
+    version='1.2.3',
     url='http://git.gisce.lan/libComXML.git',
     author='GISCE Enginyeria, SL',
     author_email='devel@gisce.net',
