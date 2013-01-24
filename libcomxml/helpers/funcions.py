c842bf799c37493f560ff45fe6c38435cd096cc7
Joan M. Grande <jgrande@el-gas.es>
2013-01-24 13:52:25 +0100

    IMP El codi de periode depen del codi dh

--- libcomxml/helpers/funcions.py
+++ libcomxml/helpers/funcions.py
@@ -27,24 +27,18 @@ def extreu_periode(name):
         return name
     return name.split('(')[-1].split(')')[0]
 
-def codi_periode(tarifa, periode):
+def codi_periode(codi_dh, periode):
     """Retorna el codi OCSUM del periode segons
     http://172.26.0.42:2500/wiki/show/Codificacio_Periodes_OCSUM
 
-    :param tarifa: codi ocsum de la tarifa
+    :param codi_dh: codi dh de la tarifa
     :param periode: nom del periode en format Px on x = {1...6}
     """
 
-    if tarifa in ('003', '011', '012', '013', '014', '015', '016'):
-        return '6%s' % (periode[-1],)
-    elif tarifa in ('001', '005'):
+    if codi_dh == '1':
         return '10'
-    elif tarifa in ('004', '006'):
-        if periode == 'P1':
-            return '01'
-        elif periode == 'P2':
-            return '03'
-
+    else:
+        return '%s%s' % (codi_dh, periode[-1])
 
 def codi_dh(tarifa, nlectures=6):
     """Retorna el codi ocsum de discriminació horaria
@@ -57,9 +51,9 @@ def codi_dh(tarifa, nlectures=6):
         return '1'
     elif tarifa in ('004', '006'):
         return '2'
-    elif tarifa in ('012', '013', '014', '015', '016'):
+    elif tarifa in ('003', '012', '013', '014', '015', '016'):
         return '6'
-    elif tarifa in ('003', '011'):
+    elif tarifa == '011':
         if nlectures == 6:
             return '6' 
         else:
