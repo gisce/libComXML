ef58f4bb9db450f1ded37083d7908ab31f8d0bd5
bmiro <bartomeumiro@gmail.com>
2013-09-23 12:04:47 +0200

    NEW Implemented D1

--- libcomxml/messages/sw_d1.py
+++ libcomxml/messages/sw_d1.py
@@ -11,30 +11,36 @@ from sw_c1 import DatosSolicitud, Contrato, Cliente, DatosAceptacion
 from sw_c1 import DatosActivacion, PuntosDeMedida
 from sw_c2 import Medida, Comentarios, RegistrosDocumento
 
+
 class NotificacionCambiosATRDesdeDistribuidor(XmlModel):
-    _sort_order = ('notificacion_cambio')
+    _sort_order = ('notificacion_cambio', 'motivo_cambio')
 
     def __init__(self):
-        self.notificacion_cambio = XmlField('NotificacionCambiosATRDesdeDistribuidor')
+        self.notificacion_cambio = XmlField(
+                                     'NotificacionCambiosATRDesdeDistribuidor'
+        )
+        self.motivo_cambio = XmlField('MotivoCambioATRDesdeDistribuidora')
         super(NotificacionCambiosATRDesdeDistribuidor, self).\
                     __init__('NotificacionCambiosATRDesdeDistribuidor',
                              'notificacion_cambio')
 
 
-class MensajeNotificacionCambiosATRDesdeDistribuidor(XmlModel):
-    _sort_order = ('mensaje', 'cabecera', 'notificacion_cambio')
+class MensajeMotivo(XmlModel):
+    _sort_order = ('mensaje', 'cabecera', 'notificacion_cambio',
+                   'periodicidad_facturacion')
 
     def __init__(self):
         self.doc_root = None
-        self.mensaje = XmlField('MensajeNotificacionCambiosATRDesdeDistribuidor', 
+        self.mensaje = XmlField('MensajeMotivo',
                           attributes={
                               'xmlns': 'http://localhost/elegibilidad'
-                           })
+                          }
+        )
         self.cabecera = Cabecera()
         self.notificacion_cambio = NotificacionCambiosATRDesdeDistribuidor()
         self.periodicidad_facturacion = XmlField('PeriodicidadFacturacion')
-        super(MensajeNotificacionCambiosATRDesdeDistribuidor, self).\
-              __init__('MensajeNotificacionCambiosATRDesdeDistribuidor', 'mensaje')
+        super(MensajeMotivo, self).\
+              __init__('MensajeMotivo', 'mensaje')
 
     def set_agente(self, agente):
         self.mensaje.attributes.update({'AgenteSolicitante': agente})
