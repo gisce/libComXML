a7edb7994433915171479629407137c746b0426e
Eduard Carreras <ecarreras@gisce.net>
2013-10-16 18:43:04 +0200

    Bump to v1.7.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 
 setup(
     name='libComXML',
-    version='1.6.0',
+    version='1.7.0',
     url='http://git.gisce.lan/libComXML.git',
     author='GISCE Enginyeria, SL',
     author_email='devel@gisce.net',
