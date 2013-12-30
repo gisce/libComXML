0ad41b6039e61cd190c8cbc4ca86e573aca64737
Eduard Carreras <ecarreras@gisce.net>
2013-12-30 16:10:40 +0100

    Update to setuptools

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
-
-from distutils.core import setup
+from setuptools import setup, find_packages
 
 setup(
     name='libComXML',
@@ -8,10 +7,9 @@ setup(
     url='http://git.gisce.lan/libComXML.git',
     author='GISCE Enginyeria, SL',
     author_email='devel@gisce.net',
-    packages=['libcomxml', 'libcomxml.core', 'libcomxml.messages',
-              'libcomxml.helpers'],
-    requires=['lxml'],
+    packages=find_packages(),
+    install_requires=['lxml'],
     license='None',
     description='libComXML',
-    long_description=open('README').read(),
+    long_description=open('README.md').read(),
 )
